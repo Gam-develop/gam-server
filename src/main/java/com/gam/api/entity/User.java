0f7033f265c17d4db267ec9fea78b380e86256cd
dragontaek-lee <dragontaek98@naver.com>
2023-06-29 01:57:43 +0900

    feat: User builder 추가 #2

--- src/main/java/com/gam/api/entity/User.java
+++ src/main/java/com/gam/api/entity/User.java
@@ -1,8 +1,6 @@
 package com.gam.api.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
+
 
 @Getter
 @Setter
@@ -67,6 +66,13 @@ public class User {
 
     @OneToMany(mappedBy = "userWork")
     List<Work> works = new ArrayList<>();
+
+    @Builder
+    public User(Role role) {
+        this.role = role;
+        this.scrapCount = 0;
+        this.viewCount = 0;
+    }
 }
 
 
