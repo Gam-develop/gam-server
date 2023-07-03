761a098758ba4fb3263168d83b55149ec1aa8ee6
dragontaek-lee <dragontaek98@naver.com>
2023-07-04 01:55:25 +0900

    feat: presigned url response #6

--- src/main/java/com/gam/api/common/ResponseMessage.java
+++ src/main/java/com/gam/api/common/ResponseMessage.java
@@ -10,7 +10,11 @@ public enum ResponseMessage {
     /** auth **/
     SUCCESS_SIGN_UP("회원 가입 성공"),
     SUCCESS_LOGIN_UP("로그인 성공"),
-    SUCCESS_GET_REFRESH_TOKEN("토큰 재발급 성공");
+    SUCCESS_GET_REFRESH_TOKEN("토큰 재발급 성공"),
+
+    /** auth **/
+    SUCCESS_GET_PRESIGNED_URL("단일 이미지 업로드 URL 가져오기 성공"),
+    SUCCESS_GET_PRESIGNED_URLS("다중 이미지 업로드 URL 가져오기 성공");
 
     private final String message;
 }
