e91206e7ed0cc08bde69946a516f9babd7fc3a39
dragontaek-lee <dragontaek98@naver.com>
2023-07-04 01:55:08 +0900

    feat: image upload extension error #6

--- src/main/java/com/gam/api/common/ExceptionMessage.java
+++ src/main/java/com/gam/api/common/ExceptionMessage.java
@@ -9,7 +9,8 @@ public enum ExceptionMessage {
     EMPTY_TOKEN("빈 토큰입니다."),
     INVALID_TOKEN("유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN("만료된 토큰입니다."),
-    INVALID_SIGNATURE("유효하지 않은 서명입니다.");
+    INVALID_SIGNATURE("유효하지 않은 서명입니다."),
+    INVALID_EXTENSION("유효하지 않은 확장자입니다.");
 
     private final String name;
 }
