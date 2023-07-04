c14dba83192794198d53be7890e61b3b48964a0c
dragontaek-lee <dragontaek98@naver.com>
2023-07-04 16:58:16 +0900

    refactor: route naming #6

--- src/main/java/com/gam/api/controller/S3Controller.java
+++ src/main/java/com/gam/api/controller/S3Controller.java
@@ -11,17 +11,17 @@ import com.gam.api.common.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/v1/image")
-public class ImageController {
+@RequestMapping("api/v1/s3")
+public class S3Controller {
     private final S3Service s3Service;
 
-    @GetMapping("")
+    @GetMapping("/image")
     public ResponseEntity<ApiResponse> getPresignedUrl(@RequestParam String fileName) {
         val response = s3Service.getPresignedUrl(fileName);
         return ResponseEntity.ok(ApiResponse.success(ResponseMessage.SUCCESS_GET_PRESIGNED_URL.getMessage(), response));
     }
 
-    @PostMapping("")
+    @PostMapping("/images")
     public ResponseEntity<ApiResponse> getPresignedUrls(@RequestBody PresignedRequestDTO request) {
         val response = s3Service.getPresignedUrls(request);
         return ResponseEntity.ok(ApiResponse.success(ResponseMessage.SUCCESS_GET_PRESIGNED_URLS.getMessage(), response));
