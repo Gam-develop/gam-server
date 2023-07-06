7483d4bdd18b2d2d9ed99bec8429c7973ebc720d
gahee99 <u1879@sookmyung.ac.kr>
2023-07-06 20:27:50 +0900

    feat: add JpaAuditingConfig

---
+++ src/main/java/com/gam/api/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.gam.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
