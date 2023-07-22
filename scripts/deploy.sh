148b716cde95ef881e8e1f2dd27ab6d086df62f9
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2023-07-22 10:26:45 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+REPOSITORY=/home/ubuntu/app
 BUILD_PATH=$(ls /home/ubuntu/app/gam-server-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
@@ -16,5 +17,19 @@ ln -Tfs $DEPLOY_PATH$JAR_NAME $IDLE_APPLICATION_PATH
 echo "> $IDLE_PROFILE 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(pgrep -f $IDLE_APPLICATION)
 
+if [ -z "$IDLE_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 $IDLE_PID
+  sleep 5
+fi
+
 echo "> $IDLE_PROFILE 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
+
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
 nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=$IDLE_PROFILE $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
