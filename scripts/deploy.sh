2ca4ec255f7eb3c0ecec1d9257dc64ab8d67e4be
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2023-07-23 15:46:09 +0900

    deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 REPOSITORY=/home/ubuntu/app
-BUILD_PATH=$(ls /home/ubuntu/app/gam-server-0.0.1-SNAPSHOT.jar)
+BUILD_PATH=$(ls /home/ubuntu/app/*SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 echo "> build 파일명: $JAR_NAME"
 
-echo "> build 파일 복사"
-DEPLOY_PATH=/home/ubuntu/app/nonstop/jar/
-cp $BUILD_PATH $DEPLOY_PATH
-
 echo "> application.jar 교체"
-IDLE_APPLICATION=$IDLE_PROFILE-gam-dev-server.jar
-IDLE_APPLICATION_PATH=$DEPLOY_PATH$IDLE_APPLICATION
+IDLE_APPLICATION=api-0.0.1-SNAPSHOT.jar
+IDLE_APPLICATION_PATH=$REPOSITORY/$IDLE_APPLICATION
 
-ln -Tfs $DEPLOY_PATH$JAR_NAME $IDLE_APPLICATION_PATH
-
-echo "> $IDLE_PROFILE 에서 구동중인 애플리케이션 pid 확인"
+echo "> $IDLE_APPLICATION 애플리케이션 pid 확인"
 IDLE_PID=$(pgrep -f $IDLE_APPLICATION)
 
 if [ -z "$IDLE_PID" ]; then
@@ -25,11 +19,5 @@ else
   sleep 5
 fi
 
-echo "> $IDLE_PROFILE 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/*SNAPSHOT.jar | tail -n 1)
-
-echo "> $JAR_NAME 에 실행권한 추가"
-chmod +x $JAR_NAME
-
 echo "> $JAR_NAME 실행"
-nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=$IDLE_PROFILE $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
+nohup java -jar -Duser.timezone=Asia/Seoul -Dspring.profiles.active=$IDLE_APPLICATION $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
