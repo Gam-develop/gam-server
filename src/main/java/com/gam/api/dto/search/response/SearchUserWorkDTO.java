27d7c7a48381a8bcae725d70bd5a32625ba34aae
gahee99 <u1879@sookmyung.ac.kr>
2023-07-27 21:39:15 +0900

    chore: Dto 생성 로직에 빼먹은 field 추가 #65

--- src/main/java/com/gam/api/dto/search/response/SearchUserWorkDTO.java
+++ src/main/java/com/gam/api/dto/search/response/SearchUserWorkDTO.java
@@ -13,6 +13,7 @@ public record SearchUserWorkDTO(
     public static SearchUserWorkDTO of (Work work) {
         return SearchUserWorkDTO.builder()
                 .thumbNail(work.getPhotoUrl())
+                .title(work.getTitle())
                 .userName(work.getUser().getUserName())
                 .viewCount(work.getViewCount())
                 .build();
