8a8749e46001e89cf8115011ee01c36dc01d1350
gahee99 <u1879@sookmyung.ac.kr>
2023-07-28 10:05:38 +0900

    chore: respository Search logic 코드리뷰 반영 #67

--- src/main/java/com/gam/api/repository/MagazineRepository.java
+++ src/main/java/com/gam/api/repository/MagazineRepository.java
@@ -12,7 +12,6 @@ public interface MagazineRepository extends JpaRepository<Magazine, Long> {
     Optional<Magazine> getMagazineById(Long magazineId);
     List<Magazine> findTop4ByOrderByCreatedAtDesc();
     List<Magazine> findTop3ByOrderByViewCountDesc();
-    List<Magazine> findAllByInterviewPersonContainingIgnoreCaseOrMagazineTitleContainingIgnoreCaseOrderByCreatedAtDesc(String userNameKeyWord, String titleNameKeyWord);
     @Query("SELECT m FROM Magazine m WHERE LOWER(m.interviewPerson) LIKE %:interviewPersonKeyword% OR LOWER(m.magazineTitle) LIKE %:magazineTitleKeyword% ORDER BY m.createdAt DESC")
     List<Magazine> finAllByKeyword(
             @Param("interviewPersonKeyword") String userNameKeyWord,
