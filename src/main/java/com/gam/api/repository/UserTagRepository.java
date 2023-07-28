419b6e85e390e53b69dc75b00abd78ba2440ac7f
gahee99 <u1879@sookmyung.ac.kr>
2023-07-28 10:04:46 +0900

    chore: 필요없는 코드 삭제 #67

--- src/main/java/com/gam/api/repository/UserTagRepository.java
+++ src/main/java/com/gam/api/repository/UserTagRepository.java
@@ -8,8 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
-    @EntityGraph(attributePaths = "tag")
-    List<UserTag> findAllByUser_Id(Long userId);
-
     void deleteAllByUser_id(Long userId);
 }
