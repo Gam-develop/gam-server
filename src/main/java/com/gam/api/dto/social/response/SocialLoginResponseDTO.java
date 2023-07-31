d53489306237e8d69e58944fff89c8a4a5ce4833
dragontaek-lee <dragontaek98@naver.com>
2023-08-01 02:46:14 +0900

    fix: login response DTO with gam version #70

--- src/main/java/com/gam/api/dto/social/response/SocialLoginResponseDTO.java
+++ src/main/java/com/gam/api/dto/social/response/SocialLoginResponseDTO.java
@@ -3,13 +3,14 @@ package com.gam.api.dto.social.response;
 import lombok.Builder;
 
 @Builder
-public record SocialLoginResponseDTO(Boolean isUser, Boolean isProfileCompleted, Long id, String accessToken) {
-    public static SocialLoginResponseDTO of(Boolean isUser,  Boolean isProfileCompleted, Long id, String accessToken) {
+public record SocialLoginResponseDTO(Boolean isUser, Boolean isProfileCompleted, Long id, String accessToken, String appVersion) {
+    public static SocialLoginResponseDTO of(Boolean isUser,  Boolean isProfileCompleted, Long id, String accessToken, String appVersion) {
         return SocialLoginResponseDTO.builder()
                 .isUser(isUser)
                 .isProfileCompleted(isProfileCompleted)
                 .id(id)
                 .accessToken(accessToken)
+                .appVersion(appVersion)
                 .build();
     }
 }
