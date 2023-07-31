b6f24e0b50fe9b5a7c46a963d707fe528b8b1bba
dragontaek-lee <dragontaek98@naver.com>
2023-08-01 02:40:34 +0900

    feat: gam config #70

---
+++ src/main/java/com/gam/api/config/GamConfig.java
@@ -0,0 +1,21 @@
+package com.gam.api.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Configuration
+public class GamConfig {
+    @Value("${gam.app.version}")
+    private String appVersion;
+
+    @Value("${gam.url.intro}")
+    private String introUrl;
+
+    @Value("${gam.url.agreement}")
+    private String agreementUrl;
+
+    @Value("${gam.url.policy}")
+    private String policyUrl;
+}
