21d41a4bf3f48c9a30f9c096eef7348cc01de3b2
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2023-08-05 18:33:26 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,10 +20,10 @@ echo "> Target port is  ${TARGET_PORT}."
 
 TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
 
-# if [ ! -z ${TARGET_PID} ]; then
-#   echo "> Kill WAS running at ${TARGET_PORT}."
-#   sudo kill ${TARGET_PID}
-# fi
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
 
 nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=dev $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
 
