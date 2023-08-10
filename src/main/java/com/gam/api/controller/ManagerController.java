0e51c0506ac1a584f7a7ebd506898b54f2943cf1
gahee99 <u1879@sookmyung.ac.kr>
2023-08-10 15:58:24 +0900

    fix: 필요없는 import 제거

--- src/main/java/com/gam/api/controller/ManagerController.java
+++ src/main/java/com/gam/api/controller/ManagerController.java
@@ -2,16 +2,11 @@ package com.gam.api.controller;
 
 import com.gam.api.common.ApiResponse;
 import com.gam.api.common.message.ResponseMessage;
-import com.gam.api.entity.GamUserDetails;
-import com.gam.api.repository.UserRepository;
-import com.gam.api.service.magazine.MagazineService;
 import com.gam.api.service.manager.ManagerService;
 import com.gam.api.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
