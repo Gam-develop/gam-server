ad8000b771d9693795eea56502f5bd3dfd7b2a9f
gahee99 <u1879@sookmyung.ac.kr>
2023-08-10 15:56:31 +0900

    fix: 필요없는 인자들 제거

--- src/main/java/com/gam/api/controller/ManagerController.java
+++ src/main/java/com/gam/api/controller/ManagerController.java
@@ -3,11 +3,14 @@ package com.gam.api.controller;
 import com.gam.api.common.ApiResponse;
 import com.gam.api.common.message.ResponseMessage;
 import com.gam.api.entity.GamUserDetails;
+import com.gam.api.repository.UserRepository;
 import com.gam.api.service.magazine.MagazineService;
 import com.gam.api.service.manager.ManagerService;
+import com.gam.api.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ManagerController {
 
     private final ManagerService managerService;
+    private final UserService userService;
 
     @GetMapping("/magazine/list")
-    public ResponseEntity<ApiResponse> getMagazines(@AuthenticationPrincipal GamUserDetails userDetails) {
+    public ResponseEntity<ApiResponse> getMagazines() {
         val response = managerService.getMagazines();
         return ResponseEntity.ok(ApiResponse.success(ResponseMessage.SUCCESS_GET_MAGAZINE_LIST.getMessage(), response));
     }
