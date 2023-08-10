cc2e4d421f52dc12a800a42731efabb759c04a60
gahee99 <u1879@sookmyung.ac.kr>
2023-08-10 15:18:48 +0900

    feat: manager - magazineList DTO 추가

---
+++ src/main/java/com/gam/api/dto/manager/magazine/response/MagazineListResponseDTO.java
@@ -0,0 +1,20 @@
+package com.gam.api.dto.manager.magazine.response;
+
+import com.gam.api.entity.Magazine;
+import lombok.Builder;
+
+
+@Builder
+public record MagazineListResponseDTO (
+        Long magazineId,
+        String magazineTitle,
+        String interviewee
+){
+    public static MagazineListResponseDTO of(Magazine magazine) {
+        return MagazineListResponseDTO.builder()
+                .magazineId(magazine.getId())
+                .magazineTitle(magazine.getMagazineTitle())
+                .interviewee(magazine.getInterviewPerson())
+                .build();
+    }
+}
