0c21940c5f2f18605e0927a1e79bf8a2018b7e7f
gahee99 <u1879@sookmyung.ac.kr>
2023-08-17 15:16:51 +0900

    feat: MagazinePhotoRepository, QuestionRepository 생성 #82

---
+++ src/main/java/com/gam/api/repository/MagazinePhotoRepository.java
@@ -0,0 +1,7 @@
+package com.gam.api.repository;
+
+import com.gam.api.entity.MagazinePhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MagazinePhotoRepository extends JpaRepository<MagazinePhoto, Long> {
+}
