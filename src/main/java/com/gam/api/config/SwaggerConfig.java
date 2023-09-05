63f628c3878cb57bcb75ce6e2ed492b78b79af02
gahee99 <u1879@sookmyung.ac.kr>
2023-09-05 14:13:01 +0900

    fix: fix swagger error

--- src/main/java/com/gam/api/config/SwaggerConfig.java
+++ src/main/java/com/gam/api/config/SwaggerConfig.java
@@ -3,13 +3,14 @@ package com.gam.api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
+@EnableWebMvc
 @Configuration
 public class SwaggerConfig {
 
@@ -17,6 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
