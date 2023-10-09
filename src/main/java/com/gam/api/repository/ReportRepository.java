7af26435e912f5b555f09115d850568e71152314
gahee99 <u1879@sookmyung.ac.kr>
2023-10-09 21:00:46 +0900

    feat: 유저 신고하기 api개발 #100

---
+++ src/main/java/com/gam/api/repository/ReportRepository.java
@@ -0,0 +1,8 @@
+package com.gam.api.repository;
+
+import com.gam.api.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
---
+++ src/main/java/com/gam/api/service/report/ReportServiceImpl.java
@@ -0,0 +1,49 @@
+package com.gam.api.service.report;
+
+import com.gam.api.common.message.ExceptionMessage;
+import com.gam.api.dto.report.request.ReportCreateRequestDTO;
+import com.gam.api.entity.Report;
+import com.gam.api.entity.User;
+import com.gam.api.entity.Work;
+import com.gam.api.repository.ReportRepository;
+import com.gam.api.repository.UserRepository;
+import com.gam.api.repository.WorkRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.val;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+@Service
+@RequiredArgsConstructor
+public class ReportServiceImpl implements ReportService{
+    private final UserRepository userRepository;
+    private final WorkRepository workRepository;
+    private final ReportRepository reportRepository;
+
+    @Transactional
+    @Override
+    public void createReport(ReportCreateRequestDTO request) {
+        val targetUser = findUser(request.targetUserId());
+        val reportedWork = findWork(request.workId());
+
+        val report = Report.builder()
+                                    .targetUser(targetUser)
+                                    .content(request.content())
+                                    .work(reportedWork)
+                                    .build();
+
+        reportRepository.save(report);
+    }
+
+    private User findUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND_USER.getMessage()));
+    }
+
+    private Work findWork(Long workId) {
+        return workRepository.findById(workId)
+                .orElseThrow(() -> new EntityNotFoundException(ExceptionMessage.NOT_FOUND_WORK.getMessage()));
+    }
+}
