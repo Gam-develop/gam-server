3b1fb9e18e4b47cb3266f6ececa6c9dbca0715c0
gahee99 <u1879@sookmyung.ac.kr>
2023-10-09 21:23:23 +0900

    fix: 신고 api개발에 따른 인기 유저 api 로직 변경 #100

--- src/main/java/com/gam/api/repository/UserRepository.java
+++ src/main/java/com/gam/api/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.gam.api.repository;
 
 import com.gam.api.entity.Magazine;
 import com.gam.api.entity.User;
+import com.gam.api.entity.UserStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,7 +13,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> getUserById(Long userId);
     boolean existsByUserName(String userName);
-    List<User> findTop5ByOrderByScrapCountDesc();
+    List<User> findTop5ByUserStatusNotOrderByScrapCountDesc(UserStatus userStatus);
     List<User> findAllByIdNotOrderBySelectedFirstAtDesc(Long id);
     @Query(value = "SELECT u FROM User u WHERE LOWER(u.userName) LIKE %:keyword% ORDER BY u.createdAt DESC")
     List<User> findByUserName(@Param("keyword") String keyword);
