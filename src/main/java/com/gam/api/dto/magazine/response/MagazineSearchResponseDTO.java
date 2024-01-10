e5c328abfc186c459386fcdb7780466b59889cb4
김주영 <gimjuyeong0620@gmail.com>
2024-01-10 16:42:53 +0900

    fix: magazine URL 상수로 추출

--- src/main/java/com/gam/api/dto/magazine/response/MagazineSearchResponseDTO.java
+++ src/main/java/com/gam/api/dto/magazine/response/MagazineSearchResponseDTO.java
@@ -3,6 +3,7 @@ package com.gam.api.dto.magazine.response;
 import com.gam.api.entity.Magazine;
 import lombok.Builder;
 
+
 @Builder
 public record MagazineSearchResponseDTO (
         String thumbNail,
@@ -11,12 +12,14 @@ public record MagazineSearchResponseDTO (
         String magazineUrl,
         Long viewCount
 ){
+    static String baseUrl = "https://flourishing-cocada-40a6ce.netlify.app/magazine/";
+
     public static MagazineSearchResponseDTO of(Magazine magazine) {
         return MagazineSearchResponseDTO.builder()
                 .thumbNail(magazine.getThumbNail())
                 .title(magazine.getMagazineTitle())
                 .interviewPerson(magazine.getInterviewPerson())
-                .magazineUrl("https://flourishing-cocada-40a6ce.netlify.app/magazine/"+magazine.getId())
+                .magazineUrl(baseUrl+magazine.getId())
                 .viewCount(magazine.getViewCount())
                 .build();
     }
