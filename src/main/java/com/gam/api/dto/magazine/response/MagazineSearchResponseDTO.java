212da17ee0f8cdb371cff76cee8abb591412d390
김주영 <gimjuyeong0620@gmail.com>
2024-01-10 16:19:47 +0900

    fix: magazine URL response에 추가

--- src/main/java/com/gam/api/dto/magazine/response/MagazineSearchResponseDTO.java
+++ src/main/java/com/gam/api/dto/magazine/response/MagazineSearchResponseDTO.java
@@ -8,6 +8,7 @@ public record MagazineSearchResponseDTO (
         String thumbNail,
         String title,
         String interviewPerson,
+        String magazineUrl,
         Long viewCount
 ){
     public static MagazineSearchResponseDTO of(Magazine magazine) {
@@ -15,6 +16,7 @@ public record MagazineSearchResponseDTO (
                 .thumbNail(magazine.getThumbNail())
                 .title(magazine.getMagazineTitle())
                 .interviewPerson(magazine.getInterviewPerson())
+                .magazineUrl("https://flourishing-cocada-40a6ce.netlify.app/magazine/"+magazine.getId())
                 .viewCount(magazine.getViewCount())
                 .build();
     }
