5a273572f09f4d16976b84ef799bdbd5124d8588
dragontaek-lee <dragontaek98@naver.com>
2024-01-16 18:56:07 +0900

    hotfix: 유저 검색 userId 추가 반환

--- src/main/java/com/gam/api/dto/search/response/SearchUserWorkDTO.java
+++ src/main/java/com/gam/api/dto/search/response/SearchUserWorkDTO.java
@@ -2,19 +2,23 @@ package com.gam.api.dto.search.response;
 
 import com.gam.api.entity.Work;
 import lombok.Builder;
+import lombok.val;
 
 @Builder
 public record SearchUserWorkDTO(
         String thumbNail,
         String title,
         String userName,
+        Long userId,
         int viewCount
 ) {
     public static SearchUserWorkDTO of (Work work) {
+        val user = work.getUser();
         return SearchUserWorkDTO.builder()
                 .thumbNail(work.getPhotoUrl())
                 .title(work.getTitle())
-                .userName(work.getUser().getUserName())
+                .userName(user.getUserName())
+                .userId(user.getId())
                 .viewCount(work.getViewCount())
                 .build();
     }
