4a43d8e4b5209767ce7644f0e2b5fa1a54a0fdc6
gahee99 <u1879@sookmyung.ac.kr>
2024-01-19 21:54:48 +0900

    feat: WorkController Swagger 명세 추가

--- src/main/java/com/gam/api/controller/WorkController.java
+++ src/main/java/com/gam/api/controller/WorkController.java
@@ -7,6 +7,7 @@ import com.gam.api.dto.work.request.WorkDeleteRequestDTO;
 import com.gam.api.dto.work.request.WorkFirstAssignRequestDto;
 import com.gam.api.entity.GamUserDetails;
 import com.gam.api.service.work.WorkService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,7 @@ import com.gam.api.common.ApiResponse;
 public class WorkController {
     private final WorkService workService;
 
+    @ApiOperation(value = "마이페이지 - 작업물 추가")
     @PostMapping("")
     public ResponseEntity<ApiResponse> createWork(
             @AuthenticationPrincipal GamUserDetails userDetails,
@@ -30,6 +32,7 @@ public class WorkController {
         return ResponseEntity.ok(ApiResponse.success(ResponseMessage.SUCCESS_CREATE_WORK.getMessage(), response));
     }
 
+    @ApiOperation(value = "마이페이지 - 작업물 삭제")
     @DeleteMapping("")
     public ResponseEntity<ApiResponse> deleteWork(
             @AuthenticationPrincipal GamUserDetails userDetails,
@@ -39,6 +42,7 @@ public class WorkController {
         return ResponseEntity.ok(ApiResponse.success(ResponseMessage.SUCCESS_DELETE_WORK.getMessage(), response));
     }
 
+    @ApiOperation(value = "대표 작업물로 설정")
     @PatchMapping("/main")
     public ResponseEntity<ApiResponse> updateFirstWork(
             @AuthenticationPrincipal GamUserDetails userDetails,
@@ -48,6 +52,7 @@ public class WorkController {
         return ResponseEntity.ok(ApiResponse.success(ResponseMessage.SUCCESS_UPDATE_FIRST_WORK.getMessage()));
     }
 
+    @ApiOperation(value = "마이페이지 - 작업물 수정")
     @PatchMapping("/edit")
     public ResponseEntity<ApiResponse> updateWork(
             @AuthenticationPrincipal GamUserDetails userDetails,
