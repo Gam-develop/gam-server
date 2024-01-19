899c5b4a86c12a23374bc48e28e688575eefc1ac
gahee99 <u1879@sookmyung.ac.kr>
2024-01-19 22:08:24 +0900

    fix: UserController -> @RestController 어노테이션 변경

--- src/main/java/com/gam/api/controller/UserController.java
+++ src/main/java/com/gam/api/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
