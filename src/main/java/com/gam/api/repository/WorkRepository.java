275d426f6e4cb42f6d85437555258025d5e9647e
김주영 <gimjuyeong0620@gmail.com>
2024-01-27 21:15:10 +0900

    feat : fetch join 제외

--- src/main/java/com/gam/api/repository/WorkRepository.java
+++ src/main/java/com/gam/api/repository/WorkRepository.java
@@ -3,6 +3,7 @@ package com.gam.api.repository;
 import com.gam.api.entity.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +13,6 @@ public interface WorkRepository extends JpaRepository<Work, Long> {
     void deleteById(Long workId);
     Optional<Work> getWorkByUserIdAndIsFirst(Long userId, Boolean status);
     List<Work> findByUserIdAndIsFirstOrderByCreatedAtDesc(Long userId, boolean isFirst);
-    @Query("SELECT w FROM Work W JOIN FETCH w.user u WHERE u.id = : userId")
     List<Work> findAllByUserId(Long userId);
     @Query(value = "SELECT w FROM Work w WHERE LOWER(w.title) LIKE LOWER(CONCAT('%', :keyword, '%')) ORDER BY w.createdAt DESC")
     List<Work> findByKeyword(String keyword);
