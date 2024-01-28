268ae162799cbd5dcb2384fe1e99ebbb17224358
dragontaek-lee <dragontaek98@naver.com>
2024-01-28 22:15:03 +0900

    hotfix: kakao 제공 정보 추가 동기화

--- src/main/java/com/gam/api/dto/kakao/KakaoUserResponse.java
+++ src/main/java/com/gam/api/dto/kakao/KakaoUserResponse.java
@@ -4,4 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public record KakaoUserResponse(String id, String connected_at) {}
+public record KakaoUserResponse(String id, String connected_at, boolean setPrivacyInfo, KakaoUserAccountResponse kakao_account) {}
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+record KakaoUserAccountResponse(boolean has_age_range, boolean age_range_needs_agreement, boolean has_gender, boolean gender_needs_agreement) {}
