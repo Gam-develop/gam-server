305bf822c391b3e529d5739782046271d4275c69
gahee99 <u1879@sookmyung.ac.kr>
2024-02-23 14:30:24 +0900

    fix: @Valid관련 오류 메시지들 추가

--- src/main/java/com/gam/api/domain/work/dto/request/WorkEditRequestDTO.java
+++ src/main/java/com/gam/api/domain/work/dto/request/WorkEditRequestDTO.java
@@ -6,9 +6,9 @@ import javax.validation.constraints.NotBlank;
 
 public record WorkEditRequestDTO(
         Long workId,
-        @NotBlank
+        @NotBlank(message = "작업물 이미지를 추가해주세요")
         String image,
-        @NotBlank
+        @NotBlank(message = "작업물 제목은 빈스트링 일 수 없습니다.")
         String title,
         String detail
 ) {
