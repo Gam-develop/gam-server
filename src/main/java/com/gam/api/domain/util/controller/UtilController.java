8e64621ad82c5844bfe8023c258fd43464e7ee2f
gahee99 <u1879@sookmyung.ac.kr>
2024-03-12 15:50:07 +0900

    feat: 크롤러 차단 #157

--- src/main/java/com/gam/api/domain/util/controller/UtilController.java
+++ src/main/java/com/gam/api/domain/util/controller/UtilController.java
@@ -1,19 +1,22 @@
-package com.gam.api.domain.test.controller;
+package com.gam.api.domain.util.controller;
 
 import com.gam.api.common.ApiResponse;
 import io.swagger.annotations.ApiOperation;
+import java.io.IOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class UtilController {
     private final Environment env;
 
     @ApiOperation(value = "heal-check 컨트롤러")
@@ -21,4 +24,17 @@ public class TestController {
     public ResponseEntity<ApiResponse> test() {
         return ResponseEntity.ok(ApiResponse.success("Hello gam Server!"));
     }
+
+    @GetMapping(value = "/robots.txt")
+    @ResponseBody
+    public void robotsBlock(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            response.getWriter().write("User-agent: *\nDisallow: /\n");
+        } catch (IOException e) {
+            log.info("exception", e);
+        }
+    }
+
+
+
 }
