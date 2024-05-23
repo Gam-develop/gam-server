9be12512a0e7dbd1dde87253e1063a893b9ab8a9
dragontaek-lee <dragontaek98@naver.com>
2024-05-24 00:31:47 +0900

    fix: type 받도록 수정, 파일명 제외

--- src/main/java/com/gam/api/domain/s3/dto/request/PresignedRequestDTO.java
+++ src/main/java/com/gam/api/domain/s3/dto/request/PresignedRequestDTO.java
@@ -2,5 +2,5 @@ package com.gam.api.domain.s3.dto.request;
 
 import java.util.List;
 
-public record PresignedRequestDTO(List<String> fileNames) {
+public record PresignedRequestDTO(List<String> fileNames, String type) {
 }
