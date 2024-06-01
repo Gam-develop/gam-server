57c3c1b5f1bb0d2a6222e15256bc2ea6f60ec33c
dragontaek-lee <dragontaek98@naver.com>
2024-06-01 12:13:15 +0900

    feat: 기존 사용 포트 종료 전략 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,9 @@ JAR_NAME=$(basename $BUILD_PATH)
 IDLE_APPLICATION_PATH=/home/ubuntu/app/${JAR_NAME}
 TARGET_PORT=8080
 
+CURRENT_PID=$(lsof -Fp -i TCP:${CURRENT_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+sudo kill ${CURRENT_PID}
+
 if [ "$DEPLOYMENT_GROUP_NAME" == "prod" ]
 then
   nohup java -jar -Duser.timezone=Asia/Seoul -Dserver.port=${TARGET_PORT} -Dspring.profiles.active=prod $IDLE_APPLICATION_PATH >> /home/ubuntu/app/nohup.out 2>&1 &
